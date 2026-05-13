25565fffc03b8700a3e3088e41a89c1b039c2531
Taofick <mdtaofick45418@gmail.com>
2026-05-13 23:06:13 +0600

    feat: add order and order item models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.conf import settings
+from decimal import Decimal
+from apps.products.models import ProductVariant
 
-# Create your models here.
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING="pending","Pending"
+        PROCESSING = "processing", "Processing"
+        SHIPPED = "shipped", "Shipped"
+        DELIVERED = "delivered", "Delivered"
+        CANCELLED = "cancelled", "Cancelled"
+    
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="orders",)
+    status=models.CharField(choices=Status.choices,default=Status.PENDING)
+    subtotal=models.DecimalField(max_digits=10,decimal_places=2,default=Decimal("0.00"))
+    total=models.DecimalField(max_digits=10,decimal_places=2,default=Decimal("0.00"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=["-created_at"]
+        indexes=[
+            models.Index(fields=["user"]),
+            models.Index(fields=["status"]),
+            models.Index(fields=["-created_at"]),
+        ]
+    
+    def __str__(self):
+        return f"Order #{self.id}"
+
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
+    variant=models.ForeignKey(ProductVariant,on_delete=models.PROTECT,related_name="order_items")
+    product_name=models.CharField(max_length=250)
+    sku=models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2,)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10,decimal_places=2,)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=["order"]),
+            models.Index(fields=["variant"]),
+        ]
+
+    def __str__(self):
+        return (
+            f"{self.product_name}"
+            f" x {self.quantity}"
+        )
+    
